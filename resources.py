8be8c0bb1a775ebf72b30f43e6718972ef6ac5a2
JaverQ <ivan.s.polevikov@gmail.com>
2022-07-29 21:48:00 +0300

    in resources.py was added 4 boolean variables and 3 button surfaces

--- resources.py
+++ resources.py
@@ -1,48 +1,45 @@
+import time
 import pygame
 import os
+import random
+import sys
 
 HEIGHT = 600
 WIDTH = 600
 
 current_volume = 0.05
 flag = True
+flag1 = True
+menu_proc = True
+opt_proc = True
+game_proc = True
+
+pygame.init()
+pygame.mixer.init()
+screen = pygame.display.set_mode((WIDTH, HEIGHT))
+pygame.display.set_caption("Tanks")
 
 main_folder = os.path.dirname(__file__)
 image_folder = os.path.join(main_folder, "image")
 sound_folder = os.path.join(main_folder, "sound")
 
-beton_img = pygame.image.load(os.path.join(image_folder, 'landscape/beton.png'))
-brick_img = pygame.image.load(os.path.join(image_folder, 'landscape/brick.png'))
-forest_img = pygame.image.load(os.path.join(image_folder, 'landscape/forest.png'))
-pol_img = pygame.image.load(os.path.join(image_folder, 'landscape/pol.png'))
-water_img = pygame.image.load(os.path.join(image_folder, 'landscape/water.png'))
-
-fon1_img = pygame.image.load(os.path.join(image_folder, 'background1.jpg'))
-fon2_img = pygame.image.load(os.path.join(image_folder, 'background2.jpg'))
-
-playerV1_img = pygame.image.load(os.path.join(image_folder, 'player/tankv1.png'))
-playerV2_img = pygame.image.load(os.path.join(image_folder, 'player/tankv2.png'))
-playerV3_img = pygame.image.load(os.path.join(image_folder, 'player/tankv3.png'))
-playerV4_img = pygame.image.load(os.path.join(image_folder, 'player/tankv4.png'))
-
-play_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/play_button.png'))
-options_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/options_button.png'))
-quit_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/quit_button.png'))
-volume_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/volume_button.png'))
-plus_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/plus_button.png'))
-minus_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/minus_button.png'))
-back_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/back_button.png'))
-left_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/left_button.png'))
-right_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/right_button.png'))
-fire_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/fire_button.png'))
-wasd_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/wasd_button.png'))
-arrows_button_img = pygame.image.load(os.path.join(image_folder, 'buttons/arrows_button.png'))
-
-vrag1_img = pygame.image.load(os.path.join(image_folder, 'enemies/vrag1.png'))
-vrag2_img = pygame.image.load(os.path.join(image_folder, 'enemies/vrag2.png'))
-vrag3_img = pygame.image.load(os.path.join(image_folder, 'enemies/vrag3.png'))
-
+beton_img = pygame.image.load(os.path.join(image_folder, 'beton.png'))
+play_button_img = pygame.image.load(os.path.join(image_folder, 'play_button.png'))
+options_button_img = pygame.image.load(os.path.join(image_folder, 'options_button.png'))
+quit_button_img = pygame.image.load(os.path.join(image_folder, 'quit_button.png'))
+fon1_img = pygame.image.load(os.path.join(image_folder, 'fon1.png'))
+player_img = pygame.image.load(os.path.join(image_folder, 'ggu.png'))
+volume_button_img = pygame.image.load(os.path.join(image_folder, 'volume_button.png'))
+plus_button_img = pygame.image.load(os.path.join(image_folder, 'plus_button.png'))
+minus_button_img = pygame.image.load(os.path.join(image_folder, 'minus_button.png'))
+back_button_img = pygame.image.load(os.path.join(image_folder, 'back_button.png'))
+left_button_img = pygame.image.load(os.path.join(image_folder, 'left_button.png'))
+right_button_img = pygame.image.load(os.path.join(image_folder, 'right_button.png'))
+fire_button_img = pygame.image.load(os.path.join(image_folder, 'fire_button.png'))
+wasd_button_img = pygame.image.load(os.path.join(image_folder, 'wasd_button.png'))
+arrows_button_img = pygame.image.load(os.path.join(image_folder, 'arrows_button.png'))
+vrag1_img = pygame.image.load(os.path.join(image_folder, 'vrag1.png'))
 missile_img = pygame.image.load(os.path.join(image_folder, 'missile.png'))
-
-fon_music = os.path.join(sound_folder, 'Main_Theme.mp3')
-battle1_music = os.path.join(sound_folder, 'Battle1.mp3')
+continue_button_img = pygame.image.load(os.path.join(image_folder, 'continue_button.png'))
+newgame_button_img = pygame.image.load(os.path.join(image_folder, 'newgame_button.png'))
+menu_button_img = pygame.image.load(os.path.join(image_folder, 'menu_button.png'))
